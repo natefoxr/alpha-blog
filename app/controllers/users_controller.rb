bf13d4bea86f8b916fddbea0415e34a6a73ae60b
natefoxr <nate@foxr.xyz>
2022-08-20 23:52:51 -0700

    Add edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,35 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def new
     @user = User.new
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(params.require(:user).permit(:username, :email, :password))
+      flash[:success] = "Account was updated successfully"
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
   def create
-    @user = User.new(params.permit(:username, :email, :password))
+    @user = User.new(params.require(:user).permit(:username, :email, :password))
     if @user.save
-      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
-      redirect_to articles_path
+      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}"
+      redirect_to root_path
     else
       render 'new'
     end
+  end
+
+  private # private should always be toward the bottom of your code
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
-  get 'signup', to: 'users#new'
-  post 'signup', to: 'users#create'
+  resources :users
 end
