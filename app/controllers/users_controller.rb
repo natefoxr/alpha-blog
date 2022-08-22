c63823e143d3bb2d44a08581d01b8e64cc91d217
natefoxr <nate@foxr.xyz>
2022-08-21 22:45:57 -0700

    Add user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   def new
     @user = User.new
   end
