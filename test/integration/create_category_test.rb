8ea9bb430e57acd2f3bd54c81c6173dae012fecb
natefoxr <nate@foxr.xyz>
2022-08-23 15:45:51 -0700

    Add integration test for invalid category

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
 
+  # Check new category is created and views/categories/show.html.erb for the category name 
   test "get the new category and create category" do
     get "/categories/new"
     assert_response :success
@@ -12,6 +13,19 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_response :success
     assert_match "Sports", response.body
+  end
+
+  # Check new category is created and views/categories/show.html.erb does not show invalid category name 
+  test "get the new category and reject invalid category submission" do
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference "Category.count" do
+      post categories_path, params: { category: { name: " " }}
+    end
+    assert_match "saved", response.body
+    assert_select "div.alert"
+    assert_select "h3.alert-heading"
+    assert_select "button.btn-close"
   end
 
 end
