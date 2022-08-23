a062b55094b242a70a7ad9580aa9af250afe666f
natefoxr <nate@foxr.xyz>
2022-08-22 21:27:40 -0700

    Add login form layout and build session controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles
   resources :users
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
